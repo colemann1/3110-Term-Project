4327fcaa346be63e5eead7b6793827912f8717bb
colemann1 <colemann1@etsu.edu>
2025-04-26 16:13:33 -0400

    Add project files.

---
+++ 3110-Term-Project/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using _3110_Term_Project.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _3110_Term_Project.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
